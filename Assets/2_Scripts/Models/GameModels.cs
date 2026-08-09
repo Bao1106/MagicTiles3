d106c563c1910b13a8b23b93105b2452ff1a58f5
Bao Vo <quocbaovo06@gmail.com>
2026-08-09 22:51:00 +0700

    import asset

---
+++ Assets/2_Scripts/Models/GameModels.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Menu,
+    Playing,
+    Paused,
+    GameOver
+}
+
+public enum Judgement
+{
+    Perfect,
+    Good,
+    Miss
+}
